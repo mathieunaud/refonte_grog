dfeb7745b0c7fab298f3a75f1eff2308e68d3a0c
mathieunaud <mathieutnaud@gmail.com>
2024-10-16 14:09:48 +0200

    update class hybride

--- Env_test/src/dsi/pms/grog/refonte/env_test/Class_medicament.java
+++ Env_test/src/dsi/pms/grog/refonte/env_test/Class_medicament.java
@@ -6,12 +6,16 @@ public class Class_medicament {
     private String EEN;
     private String AMM;
     private String groupegene;
+    private String titulaire;
+    private String voieinal;
 
-    public Class_medicament(String libelle, int codeCIS, String EEN, String AMM, String groupegene) {
+    public Class_medicament(String libelle, int codeCIS, String EEN, String AMM, String groupegene, String titulaire, String voieinal) {
         this.libelle = libelle;
         this.codeCIS = codeCIS;
         this.EEN = EEN;
         this.AMM = AMM;
+        this.titulaire = titulaire;
+        this.voieinal = voieinal;
     }
 
     // Getters
@@ -34,12 +38,20 @@ public class Class_medicament {
     public String getGroupegene() {
     	return groupegene;
     }
+    
+    public String gettitulaire() {
+    	return titulaire;
+    }
+    
+    public String getvoieinal() {
+    	return voieinal;
+    }
 
     public class Class_Hybride extends Class_medicament {
         private String situationMedicale;
 
-        public Class_Hybride(String libelle, int codeCIS, String EEN, String AMM, String situationMedicale, String groupegene) {
-            super(libelle, codeCIS, EEN, AMM, groupegene);
+        public Class_Hybride(String libelle, int codeCIS, String EEN, String AMM, String situationMedicale, String groupegene, String titulaire, String voieinal) {
+            super(libelle, codeCIS, EEN, AMM, groupegene, titulaire, voieinal);
             this.situationMedicale = situationMedicale;
         }
 
@@ -51,8 +63,8 @@ public class Class_medicament {
     public class Class_Generique extends Class_medicament {
         private String SpeRef;
 
-        public Class_Generique(String libelle, int codeCIS, String EEN, String AMM, String situationMedicale, String groupegene) {
-            super(libelle, codeCIS, EEN, AMM, groupegene);
+        public Class_Generique(String libelle, int codeCIS, String EEN, String AMM, String situationMedicale, String groupegene, String titulaire, String voieinal) {
+            super(libelle, codeCIS, EEN, AMM, groupegene, titulaire, voieinal);
         }
 
         // Getters
