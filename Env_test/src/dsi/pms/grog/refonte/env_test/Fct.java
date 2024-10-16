4c567194822e07ce74612b1f402784de9662280c
mathieunaud <mathieutnaud@gmail.com>
2024-10-16 12:46:56 +0200

    ajout d'un code java

---
+++ Env_test/src/dsi/pms/grog/refonte/env_test/Fct.java
@@ -0,0 +1,15 @@
+package dsi.pms.grog.refonte.env_test;
+
+
+
+
+public class Fct {
+    public static void main(String[] args) {
+        printMessage();
+    }
+
+    public static void printMessage() {
+        System.out.println("toto");
+    }
+    
+}
