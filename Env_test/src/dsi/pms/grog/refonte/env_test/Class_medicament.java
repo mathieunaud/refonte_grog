5a8231b2cc3ffbfe9ca153c9b80702e0599207c7
mathieunaud <mathieutnaud@gmail.com>
2024-10-16 13:34:53 +0200

    ajout class hybride et generique

--- Env_test/src/dsi/pms/grog/refonte/env_test/Class_medicament.java
+++ Env_test/src/dsi/pms/grog/refonte/env_test/Class_medicament.java
@@ -3,11 +3,15 @@ package dsi.pms.grog.refonte.env_test;
 public class Class_medicament {
     private String libelle;
     private int codeCIS;
+    private String EEN;
+    private String AMM;
+    private String groupegene;
 
-
-    public Class_medicament(String libelle, int codeCIS) {
+    public Class_medicament(String libelle, int codeCIS, String EEN, String AMM, String groupegene) {
         this.libelle = libelle;
         this.codeCIS = codeCIS;
+        this.EEN = EEN;
+        this.AMM = AMM;
     }
 
     // Getters
@@ -19,13 +23,23 @@ public class Class_medicament {
         return codeCIS;
     }
 
+    public String getEEN() {
+        return EEN;
+    }
+
+    public String getAMM() {
+        return AMM;
+    }
+    
+    public String getGroupegene() {
+    	return groupegene;
+    }
 
     public class Class_Hybride extends Class_medicament {
         private String situationMedicale;
 
- 
-        public Class_Hybride(String libelle, int codeCIS, String situationMedicale) {
-            super(libelle, codeCIS);
+        public Class_Hybride(String libelle, int codeCIS, String EEN, String AMM, String situationMedicale, String groupegene) {
+            super(libelle, codeCIS, EEN, AMM, groupegene);
             this.situationMedicale = situationMedicale;
         }
 
@@ -34,4 +48,17 @@ public class Class_medicament {
             return situationMedicale;
         }
     }
+    public class Class_Generique extends Class_medicament {
+        private String SpeRef;
+
+        public Class_Generique(String libelle, int codeCIS, String EEN, String AMM, String situationMedicale, String groupegene) {
+            super(libelle, codeCIS, EEN, AMM, groupegene);
+        }
+
+        // Getters
+        public String getSpeRef() {
+            return SpeRef;
+        }
+    }
+    
 }
